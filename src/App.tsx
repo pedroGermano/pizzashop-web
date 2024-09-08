6cf0a6ed430902f1bea154ad984e5b86f78ab01e
Pedro Germano <pedrogermano232@gmail.com>
2024-09-08 16:37:25 -0300

    route add

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { Button } from "./components/ui/button";
 import "./global.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
 
 export function App() {
   return (
     <>
-      <div className="flex h-screen items-center justify-center">
-        <Button>Enviar</Button>
-      </div>
+      <RouterProvider router={router} />
     </>
   );
 }
