0e76597115fa29a5d4c7f89c89b816b3b666b411
Pedro Germano <pedrogermano232@gmail.com>
2024-09-09 10:21:47 -0300

    button isSubmitting

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,11 +1,19 @@
 import { Outlet } from "react-router-dom";
+import { Pizza } from "lucide-react";
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Autenticação</h1>
-
-      <div>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="flex flex-col justify-between h-full p-10 border-r border-foreground/5 bg-muted text-muted-foreground">
+        <div className="flex items-center gap-3 text-lg text-foreground">
+          <Pizza className="w-5 h-5" />
+          <span className="font-semibold">pizza.shop</span>
+        </div>
+        <footer className="text-sm">
+          Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
+        </footer>
+      </div>
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
