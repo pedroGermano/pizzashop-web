4b95f7fe2f0d7405b5bc05742c2b9a133a0aa8af
Pedro Germano <pedrogermano232@gmail.com>
2024-09-09 12:19:30 -0300

    sign up screen

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,9 +1,11 @@
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { toast } from "sonner";
 import { Button } from "../../components/ui/button";
 import { Label } from "../../components/ui/label";
 import { Input } from "../../components/ui/input";
+import { Link } from "react-router-dom";
 
 const signInForm = z.object({
   email: z.string().email(),
@@ -21,12 +23,19 @@ export function SignIn() {
   async function handleSignIn(data: SignInForm) {
     console.log(data);
     await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    toast.success("Enviamos um link de autenticação para o seu e-mail");
   }
 
   return (
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button variant="ghost" asChild className="absolute right-8 top-8">
+          <Link className="" to="/sign-up">
+            Novo estabelecimento
+          </Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { Dashboard } from "./pages/app/dashboard";
 import { SignIn } from "./pages/auth/sign-in";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
+import { SignUp } from "./pages/auth/sign-up";
 
 export { createBrowserRouter } from "react-router-dom";
 
@@ -16,6 +17,9 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <AuthLayout />,
-    children: [{ path: "/sign-in", element: <SignIn /> }],
+    children: [
+      { path: "/sign-in", element: <SignIn /> },
+      { path: "/sign-up", element: <SignUp /> },
+    ],
   },
 ]);
