3a177196be37098a050e188f2ec2950b1a7847e6
Pedro Germano <pedrogermano232@gmail.com>
2024-09-13 17:48:35 -0300

    table create

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Dashboard } from "./pages/app/dashboard";
-import { SignIn } from "./pages/auth/sign-in";
+
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
+import { Dashboard } from "./pages/app/dashboard";
+import { Orders } from "./pages/app/orders/orders";
+import { SignIn } from "./pages/auth/sign-in";
 import { SignUp } from "./pages/auth/sign-up";
 
 export { createBrowserRouter } from "react-router-dom";
@@ -11,7 +13,10 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    children: [{ path: "/", element: <Dashboard /> }],
+    children: [
+      { path: "/", element: <Dashboard /> },
+      { path: "/orders", element: <Orders /> },
+    ],
   },
 
   {
