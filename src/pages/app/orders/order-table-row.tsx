de89d3ffb4a699b9c875ee7591fdf5dc3221207f
Pedro Germano <pedrogermano232@gmail.com>
2024-09-23 13:07:02 -0300

    cards add

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,6 +1,8 @@
 import { ArrowRight, Search, X } from "lucide-react";
 import { Button } from "../../../components/ui/button";
 import { TableCell, TableRow } from "../../../components/ui/table";
+import { Dialog, DialogTrigger } from "../../../components/ui/dialog";
+import { OrderDetails } from "./order-details";
 
 // export interface OrderTableProps {}
 
@@ -8,10 +10,16 @@ export function OrderTableRow() {
   return (
     <TableRow>
       <TableCell>
-        <Button variant="outline" size="xs">
-          <Search className="h-3 w-3" />
-          <span className="sr-only">Detalhes do pedido</span>
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+
+          <OrderDetails />
+        </Dialog>
       </TableCell>
       <TableCell className="font-mono text-xs font-medium">
         821e78f7asdhdf128h
